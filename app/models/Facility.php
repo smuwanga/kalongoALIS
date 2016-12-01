bc436497f141d92d892c859f1d7c9baca3814833
Geoffrey Anguyo <ganguyo@clintonhealthaccess.org>
2016-12-02 01:43:28 +0300

    Two additional files edited by Phillip, Justus & Geoffrey -app/models/Facility, app/models/Test

--- app/models/Facility.php
+++ app/models/Facility.php
@@ -3,4 +3,24 @@
 class Facility extends Eloquent
 {
 	protected $table = "facilities";
+
+	/**
+	*
+	* District Relationship
+	*
+	*/
+	public function district()
+	{
+		$this->hasOne('District');
+	}
+
+	/**
+	 *
+	 *FacilityLevel Relationship
+	 *
+	 */
+	 public function facilitylevel()
+	 {
+	 	$this->hasOne('FacilityLevel');
+	 }
 }
