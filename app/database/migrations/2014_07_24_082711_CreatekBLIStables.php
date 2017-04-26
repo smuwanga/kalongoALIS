0721b4fe0888b7dcf795eda005195c26ce1211a1
ereng <aereng@gmail.com>
2017-04-26 11:52:17 +0300

    to have fractions of a year, will need to improve it for sure

--- app/database/migrations/2014_07_24_082711_CreatekBLIStables.php
+++ app/database/migrations/2014_07_24_082711_CreatekBLIStables.php
@@ -150,8 +150,8 @@ class CreatekBLIStables extends Migration {
         {
             $table->increments('id')->unsigned();
             $table->integer('measure_id')->unsigned();
-            $table->integer('age_min')->unsigned()->nullable();
-            $table->integer('age_max')->unsigned()->nullable();
+            $table->decimal('age_min')->nullable();
+            $table->decimal('age_max')->nullable();
             $table->tinyInteger('gender')->unsigned()->nullable();
             $table->decimal('range_lower', 7, 3)->nullable();
             $table->decimal('range_upper', 7, 3)->nullable();
