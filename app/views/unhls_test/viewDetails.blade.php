138e80d8a5b1e476c963b54e99c58872bab69fb3
ereng <aereng@gmail.com>
2017-06-20 12:38:17 +0300

    geoff on this

--- app/views/unhls_test/viewDetails.blade.php
+++ app/views/unhls_test/viewDetails.blade.php
@@ -30,7 +30,7 @@
 							@endif
 						</div>
 						@endif
-						@if($test->isCompleted() && $test->isVerified())
+						@if($test->isCompleted() || $test->isVerified())
 						<div class="panel-btn">
 							@if(Auth::user()->can('view_reports'))
 <!-- 								<a class="btn btn-sm btn-default"
