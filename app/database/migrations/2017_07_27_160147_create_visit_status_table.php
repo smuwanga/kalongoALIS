4e8b589e8855fabd33d223d6dffce4fa31427c60
ereng <aereng@gmail.com>
2017-10-17 12:49:10 +0300

    move seeding information to migration

--- app/database/migrations/2017_07_27_160147_create_visit_status_table.php
+++ app/database/migrations/2017_07_27_160147_create_visit_status_table.php
@@ -17,6 +17,27 @@ class CreateVisitStatusTable extends Migration {
 			$table->increments('id');
 			$table->string('name',45);
 		});
+
+		// migrate year
+		Eloquent::unguard();
+
+		VisitStatus::create(["id" => 1, "name" => "appointment-made"]);
+		VisitStatus::create(["id" => 2, "name" => "test-request-made"]);
+		VisitStatus::create(["id" => 3, "name" => "specimen_received"]);
+		VisitStatus::create(["id" => 4, "name" => "tests-completed"]);
+
+		/* Permissions table */
+		$permissions = array(
+			array("name" => "manage_appointments", "display_name" => "Can manage appointments with Clinician"),
+			array("name" => "make_labrequests", "display_name" => "Can make lab requests(Only for Clinicians)"),
+			array("name" => "manage_visits", "display_name" => "Can manage visit content"),
+		);
+
+		$superadmin = Role::find(1);
+		foreach ($permissions as $permission) {
+			$superadmin->attachPermission(Permission::create($permission));
+		}
+
 	}
 
 	/**
